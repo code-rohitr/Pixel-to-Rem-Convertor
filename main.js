2c553ae72de6810cab4fd23e2b3dc92b276998be
rohit raj <official.rohitr@gmail.com>
2022-02-14 10:25:14 +0530

    Add github link

--- main.js
+++ main.js
@@ -8,11 +8,19 @@ const rem_input = document.querySelector(".rem-input")
 
 
 pxl_input.addEventListener("keyup", ()=>{
+    
+    if(isNaN(pxl_input.value)){
+        pxl_input.value = ""
+    }
 
     rem_input.value = pxl_input.value/16
 })
 
 rem_input.addEventListener("keyup", ()=>{
 
+    if(isNaN(rem_input.value)){
+        rem_input.value = ""
+    }
     pxl_input.value = 16 * rem_input.value
 })
+
